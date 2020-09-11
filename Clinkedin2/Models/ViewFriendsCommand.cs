3faa13d227817917c7f4bf145bac7faa75ebe628
Anca Simon <ancarsimon@gmail.com>
2020-09-11 00:52:06 -0500

    wip

---
+++ Clinkedin2/Models/ViewFriendsCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinkedin2.Models
+{
+    public class ViewFriendsCommand
+    {
+        public List<Inmate> Friends {get; set;}
+
+    }
+}
